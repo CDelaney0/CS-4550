b23fb76ac2e20218c4d83edbd36519cb94839a11
CDelaney0 <46506898+CDelaney0@users.noreply.github.com>
2020-03-04 14:57:20 -0700

    Create Parser.h

---
+++ Parser.h
@@ -0,0 +1,14 @@
+#ifndef Parser
+#define Parser
+
+class ParserClass{
+  public:
+  ParserClass(ScannerClass*,SymbolTableClass)
+  Start()
+  private:
+  ScannerClass* SC;
+  SymbolTableClass STC;
+
+}
+
+#endif
