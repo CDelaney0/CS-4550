986ac573ea0111be358b51a24371050af300c60f
CDelaney0 <46506898+CDelaney0@users.noreply.github.com>
2020-03-04 14:57:51 -0700

    Create Scanner.h

---
+++ Scanner.h
@@ -0,0 +1,16 @@
+#include "StateMachine.h"
+#ifndef Scanner
+#define Scanner
+
+class ScannerClass{
+  public;
+    ScannerClass(string input)
+    GetNextToken(){TokenClass current}
+    PeekNextToken()
+    
+
+  private;
+    ifstream mFin;
+}
+
+#endif 
