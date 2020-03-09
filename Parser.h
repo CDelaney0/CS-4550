4bd4f616b32ff276aae5c9846f58e9f679005105
CDelaney0 <46506898+CDelaney0@users.noreply.github.com>
2020-03-09 17:27:22 -0600

    Update Parser.h

--- Parser.h
+++ Parser.h
@@ -1,14 +1,39 @@
-#ifndef Parser
-#define Parser
+#include "node.h"
+#pragma once
+#include "scanner.h"
+#include "symbol.h"
+#include "debug.h"
+#include "token.h"
 
-class ParserClass{
-  public:
-  ParserClass(ScannerClass*,SymbolTableClass)
-  Start()
-  private:
-  ScannerClass* SC;
-  SymbolTableClass STC;
-
-}
-
-#endif
+class Parser {
+public:
+	Parser();
+	Parser(Scanner *s, SymbolTable *st);
+	~Parser();
+	ExpressionNode * Relational();
+	ExpressionNode * AND();
+	ExpressionNode * OR();
+	ExpressionNode* TimesDivide();
+	ExpressionNode* NOT();
+	ExpressionNode* Factor();
+	ExpressionNode* PlusMinus();
+	ExpressionNode* Expression();
+	IdentifierNode* Identifier();
+	IntegerNode* Integer();
+	IfNode * IF();
+	WhileNode * WHILE();
+	DoWhileNode * DoWHILE();
+	RepeatNode * Repeat();
+	DeclarationStatementNode* DeclarationStatement();
+	CoutStatementNode* CoutStatement();
+	AssignmentStatementNode* AssignmentStatement();
+	Token Match(TokenType expected);
+	StartNode* Start();
+	ProgramNode* Program();
+	BlockNode* Block();
+	StatementGroupNode* StatementGroup();
+	StatementNode* Statement();
+private:
+	Scanner *mScanner;
+	SymbolTable *mSymbolTable;
+};
