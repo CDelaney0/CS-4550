e07374b093e30387ade697d392a8dc123a274195
CDelaney0 <46506898+CDelaney0@users.noreply.github.com>
2020-03-09 17:33:05 -0600

    Update Token.h

--- Token.h
+++ Token.h
@@ -1,53 +1,49 @@
-#ifndef TOKEN
-#define TOKEN
+#ifndef _TOKEN_H_
+#define _TOKEN_H_
+#include <cstdlib>
+#include <vector>
+#include <iostream>
+#include <string>
+#include "debug.h"
 
-#include<string>
-
-enum TokenType{
-  // Reserved Words:
-  VOID_TOKEN, MAIN_TOKEN, INT_TOKEN, COUT_TOKEN,
-  // Relational Operators:
-  LESS_TOKEN, LESSEQUAL_TOKEN, GREATER_TOKEN,GREATEREQUAL_TOKEN,
-  EQUAL_TOKEN, NOTEQUAL_TOKEN,
-  // Other Operators:
-  INSERTION_TOKEN, ASSIGNMENT_TOKEN, PLUS_TOKEN, MINUS_TOKEN, TIMES_TOKEN, DIVIDE_TOKEN,
-  // Other Characters:
-  SEMICOLON_TOKEN, LPAREN_TOKEN, RPAREN_TOKEN, LCURLY_TOKEN,RCURLY_TOKEN,
-  // Other Token Types:
-  IDENTIFIER_TOKEN, INTEGER_TOKEN, BAD_TOKEN, ENDFILE_TOKEN
+const std::string gTokenTypeNames[] = {
+	"VOID", "MAIN", "INT", "COUT",
+	"LESS", "LESSEQUAL", "GREATER", "GREATEREQUAL", "EQUAL", "NOTEQUAL", "NOT",
+	"INSERTION", "ASSIGNMENT", "PLUS", "INC", "DEC", "PLUSEQUAL", "MINUS", "MINUSEQUAL", "TIMES",
+	"TIMESEQUAL", "DIVIDE", "DIVIDEEQUAL", "MODULUS", "endl",
+	"SEMICOLON", "LPAREN", "RPAREN", "LCURLY", "RCURLY",
+	"IDENTIFIER", "INTEGER", "IF", "ELSE","DO","WHILE", "AND", "OR", "STRING", "WORD", "REPEAT",
+	"BAD", "ENDFILE"
 };
-// IMPORTANT: The list above and the list below MUST be kept in sync.
-const std::string gTokenTypeNames[] = { "VOID", "MAIN", "INT", "COUT",
-"LESS", "LESSEQUAL", "GREATER", "GREATEREQUAL", "EQUAL",
-"NOTEQUAL",
-"INSERTION", "ASSIGNMENT", "PLUS", "MINUS", "TIMES", "DIVIDE",
-"SEMICOLON", "LPAREN", "RPAREN", "LCURLY", "RCURLY",
-"IDENTIFIER", "INTEGER",
-"BAD", "ENDFILE"
+enum TokenType {// Reserved Words
+	VOID_TOKEN, MAIN_TOKEN, INT_TOKEN, COUT_TOKEN,
+	// Relational Operators:
+	LESS_TOKEN, LESSEQUAL_TOKEN, GREATER_TOKEN, GREATEREQUAL_TOKEN, EQUAL_TOKEN, NOTEQUAL_TOKEN, NOT_TOKEN,
+	// Other Operators:
+	INSERTION_TOKEN, ASSIGNMENT_TOKEN, PLUS_TOKEN, INC_TOKEN, DEC_TOKEN, PLUSEQUAL_TOKEN, MINUS_TOKEN, MINUSEQUAL_TOKEN, TIMES_TOKEN, 
+	TIMESEQUAL_TOKEN, DIVIDE_TOKEN, DIVIDEEQUAL_TOKEN, MODULUS_TOKEN, ENDL_TOKEN,
+	// Other Characters:
+	SEMICOLON_TOKEN, LPAREN_TOKEN, RPAREN_TOKEN, LCURLY_TOKEN, RCURLY_TOKEN,
+	// Other Token Types:
+	IDENTIFIER_TOKEN, INTEGER_TOKEN, IF_TOKEN, ELSE_TOKEN, DO_TOKEN, WHILE_TOKEN, AND_TOKEN, OR_TOKEN, STRING_TOKEN, WORD_TOKEN, REPEAT_TOKEN,
+	BAD_TOKEN, ENDFILE_TOKEN
 };
-
-class TokenClass
+class Token 
 {
-private:
-  TokenType mType;
-  std::string mLexeme;
-public:
-  TokenClass();
-
-  TokenClass(TokenType type, const std::string & lexeme);
-
-  TokenType GetTokenType() const { return mType; }
-
-  const std::string & GetTokenTypeName() const { return gTokenTypeNames[mType]; }
-
-  std::string GetLexeme() const { return mLexeme; }
-
-  void CheckReserved();
-  
-  static const std::string & GetTokenTypeName(TokenType type) { return gTokenTypeNames[type]; }
-
+	public:
+		Token();
+		Token(TokenType type, const std::string &lexeme);
+		//std::string GetTypeString() const { return TokenType[mType]; };
+		//static std::string GetTypeString() {};
+		void CheckReserved();
+		void SetTokenType(TokenType type);
+		void SetTokenLexeme(std::string lexeme);
+		const std::string &GetTokenTypeName() const {return gTokenTypeNames[mType];}
+		TokenType GetTokenType() const { return mType; };
+		std::string GetLexeme() const { return mLexeme; };
+	private:
+		TokenType mType; 
+		std::string mLexeme;
 };
-
-std::ostream& operator<<(std::ostream& cout, const TokenClass& tc);
-
-#endif // TOKEN
+std::ostream &operator << (std::ostream & out, const Token & tok); 
+#endif
