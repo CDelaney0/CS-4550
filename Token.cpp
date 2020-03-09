febff46ca60c8947838a4892156dbc4676a1593f
CDelaney0 <46506898+CDelaney0@users.noreply.github.com>
2020-03-09 17:32:28 -0600

    Update Token.cpp

--- Token.cpp
+++ Token.cpp
@@ -1,41 +1,48 @@
 #include "token.h"
-#include <string>
-#include <sstream>
 
-TokenClass::TokenClass(){
-  mType = VOID_TOKEN;
-  mLexeme = "VOID";
-}
+Token::Token() {
+};
+Token::Token(TokenType type, const std::string &lexeme) 
+	:mType(type), mLexeme(lexeme)
+{
 
-TokenClass::TokenClass(TokenType type, const std::string & lexeme){
-  mType = type;
-  mLexeme = lexeme;
 }
-
-void TokenClass::CheckReserved(){
-  if (mLexeme == "void"){
-    mType = VOID_TOKEN;
-  }
-  else if(mLexeme == "main"){
-    mType = MAIN_TOKEN;
-  }
-  else if(mLexeme == "int"){
-    mType = INT_TOKEN;
-  }
-  else if(mLexeme == "cout"){
-    mType = COUT_TOKEN;
-  }
-  else{
-    return;
-  }
+void Token::CheckReserved() {
+	if (mLexeme == "void") {
+		mType = VOID_TOKEN;
+	}
+	if (mLexeme == "main") {
+		mType = MAIN_TOKEN;
+	}
+	if (mLexeme == "int") {
+		mType = INT_TOKEN;
+	}
+	if (mLexeme == "cout") {
+		mType = COUT_TOKEN;
+	}
+	if (mLexeme == "if") {
+		mType = IF_TOKEN;
+	}
+	if (mLexeme == "while") {
+		mType = WHILE_TOKEN;
+	}
+	if (mLexeme == "do") {
+		mType = DO_TOKEN;
+	}
+	if (mLexeme == "string") {
+		mType = STRING_TOKEN;
+	}
+	if (mLexeme == "repeat") {
+		mType = REPEAT_TOKEN;
+	}
+	if (mLexeme == "endl") {
+		mType = ENDL_TOKEN;
+	}
+	if (mLexeme == "else") {
+		mType = ELSE_TOKEN;
+	}
 }
-
-std::ostream& operator<<(std::ostream out, const TokenClass tc){
-  std::string T = "type: ";
-  std::string N = " Name: ";
-  std::string L = " Lexme: ";
-  TokenType TT = tc.GetTokenType();
-  std::string TN = tc.GetTokenTypeName();
-  std::string TL = tc.GetLexeme();
-  return out << T << TT << N << TN << L << TL << std::endl;
-}
+//void Token::SetToken(TokenType type, std::string lexeme) {};
+void Token::SetTokenLexeme(std::string lexeme) { mLexeme = lexeme; };
+void Token::SetTokenType(TokenType type) { mType = type; };
+std::ostream &operator << (std::ostream & out, const Token & tok) { std::cout << tok.GetTokenType(); return out; };
