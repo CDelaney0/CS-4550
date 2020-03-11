9b517e978de54eb0734222d4049780bd5d23fa73
CDelaney0 <46506898+CDelaney0@users.noreply.github.com>
2020-03-11 15:37:20 -0600

    Update Token.cpp

--- A2/Token.cpp
+++ A2/Token.cpp
@@ -6,3 +6,19 @@ Token::Token(TokenType type, const std::string &lexeme)
 	:mType(type), mLexeme(lexeme)
 {
 }
+void Token::CheckReserved() {
+	if (mLexeme == "void") {
+		mType = VOID_TOKEN;
+	}
+	if (mLexeme == "main") {
+		mType = MAIN_TOKEN;
+	}
+	if (mLexeme == "int") {
+		mType = INT_TOKEN;
+	}
+	if (mLexeme == "cout") {
+		mType = COUT_TOKEN;
+	}
+}
+
+std::ostream &operator << (std::ostream & out, const Token & tok) { std::cout << tok.GetTokenType(); return out; };
