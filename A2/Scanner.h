e75ea31c54980c108e3311ea31d8b04a27437083
CDelaney0 <46506898+CDelaney0@users.noreply.github.com>
2020-03-11 16:03:07 -0600

    Update Scanner.h

--- A2/Scanner.h
+++ A2/Scanner.h
@@ -1,1 +1,15 @@
+#ifndef Scanner
+#define Scanner
+#include "StateMachine.h"
+#include "Token.h"
 
+class ScannerClass{
+  public;
+    ScannerClass(string input)
+    Token GetNextToken()
+    
+  private;
+    ifstream mFin;
+}
+
+#endif
